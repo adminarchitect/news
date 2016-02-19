6018b58acd6e98ed1d0a1504d0982f3fd01d868a
endi <endi1982@gmail.com>
2016-02-19 17:03:40 +0200

    add news() helper

--- src/helpers.php
+++ src/helpers.php
@@ -1,2 +1,15 @@
 <?php
 
+use Terranet\News\Contracts\NewsRepository;
+
+if (! function_exists('news')) {
+    /**
+     * Get the news repository instance
+     *
+     * @return NewsRepository
+     */
+    function news()
+    {
+        return app()->make(NewsRepository::class);
+    }
+}
