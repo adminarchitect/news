eecfcd711962dfd3c2f45428bc3d3a3a4e488023
endi <endi1982@gmail.com>
2016-02-19 15:23:45 +0200

    Make modules and models publishable

--- src/Terranet/News/ServiceProvider.php
+++ src/Terranet/News/ServiceProvider.php
@@ -20,8 +20,6 @@ class ServiceProvider extends LaravelServiceProvider
         $local = "{$baseDir}/publishes/routes.php";
         $routes = app_path('Http/Terranet/News/routes.php');
 
-        $this->publishes([$local => $routes], 'routes');
-
         if (! $this->app->routesAreCached()) {
             if (file_exists($routes)) {
                 /** @noinspection PhpIncludeInspection */
@@ -31,22 +29,28 @@ class ServiceProvider extends LaravelServiceProvider
                 require_once $local;
             }
         }
+
+        // routes
+        $this->publishes([$local => $routes], 'routes');
+
+        // resources
+        $this->publishes(["{$baseDir}/publishes/Modules" => app_path('Http/Terranet/Administrator/Modules')], 'resources');
+
+        // models
+        $this->publishes(["{$baseDir}/publishes/Models" => app_path()], 'models');
     }
 
     public function register()
     {
-        $this->registerCommands();
-
         $this->app->register(SluggableServiceProvider::class);
+
+        $this->registerCommands();
     }
 
     protected function registerCommands()
     {
-        $this->app->singleton('Terranet\News\Contracts\NewsRepository', function() {
-            return new NewsRepository(
-                NewsItem::class,
-                NewsCategory::class
-            );
+        $this->app->singleton('Terranet\News\Contracts\NewsRepository', function () {
+            return new NewsRepository(NewsItem::class, NewsCategory::class);
         });
 
         $this->app->singleton('command.news.table', function ($app) {
