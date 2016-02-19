0e6442b09eab13f79fa387e0a6b176eff56b79e6
endi <endi1982@gmail.com>
2016-02-19 17:04:01 +0200

    Added comments

--- src/Terranet/News/Contracts/NewsRepository.php
+++ src/Terranet/News/Contracts/NewsRepository.php
@@ -2,11 +2,33 @@
 
 namespace Terranet\News\Contracts;
 
+use App\NewsItem;
+use Illuminate\Database\Eloquent\Collection;
+
 interface NewsRepository
 {
+    /**
+     * Fetch news in descending order
+     *
+     * @param int $page
+     * @return Collection
+     */
     public function recent($page = 1);
 
+    /**
+     * Find news by unique identifier
+     *
+     * @param $slug
+     * @return NewsItem
+     */
     public function findBySlug($slug);
 
+    /**
+     * Find news by category identifier
+     *
+     * @param $slug
+     * @param int $page
+     * @return Collection
+     */
     public function findByCategory($slug, $page = 1);
 }
